7b272d06e4334f13fac9e55cd4db4e8ce5128be9
CeciliaChen <cecilia@pdotf.com>
2023-09-22 01:48:46 -0400

    Initial Commit

---
+++ TaskManager/TaskManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TaskManagerApp.swift
+//  TaskManager
+//
+//  Created by Cecilia Chen on 9/22/23.
+//
+
+import SwiftUI
+
+@main
+struct TaskManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
